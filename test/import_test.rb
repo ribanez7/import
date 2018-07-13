7ac6c1e0259a84ff34c307f5ebc918e473e75ae2
Rubén Ibáñez Carmona <ruben.ibc@gmail.com>
2018-07-13 12:19:12 +0200

    test the previous functionallity

--- test/import_test.rb
+++ test/import_test.rb
@@ -8,4 +8,12 @@ class ImportTest < Minitest::Test
   def test_it_does_something_useful
     assert true
   end
+
+  def test_it_informs_when_a_filetype_is_not_supported
+    file = 'newcompany.csv'
+
+    assert_raises(NotImplementedError) do
+      Import::Import.call('newcompany', file)
+    end
+  end
 end
